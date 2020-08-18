aabc79ead17d020e6f4fc83cc0d40d9dac59b53f
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2020-08-18 17:33:49 -0400

    First Commit. This is the begin of the long journey. Good Luck.

---
+++ cmake-build-debug/CMakeFiles/my_stl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_stl.dir/main.cpp.o"
+  "my_stl"
+  "my_stl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_stl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
